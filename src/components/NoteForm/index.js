ad56e6cabd2ab9247a46f98c817ddd160c07ca0c
Rogerio Leal <rogerioleal1@gmail.com>
2019-09-01 21:24:57 -0300

    Adiciona lib react-hook-form

--- src/components/NoteForm/index.js
+++ src/components/NoteForm/index.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react'
-import { Container } from './styles'
+import React, { useEffect} from 'react'
+import useForm from 'react-hook-form'
+
+import { Container, Input } from './styles'
 
 export default function Header({ addNotes }) {
+    
+    const { register, handleSubmit, errors } = useForm()
 
-    const [title, setTitle] = useState('')
-
-    const handleTitle = e => setTitle(e.target.value)
-
-    const handleSubmit = e => {
-        e.preventDefault()
-
-        if (! title) return
-
-        addNotes(title)
-        setTitle('')
+    const onSubmit = (data, e) => {
+        addNotes(data.title)
+        e.target.reset()
     }
+
+    useEffect(() => console.log(errors), [errors])
 
     return (
         <Container>
-            <form onSubmit={handleSubmit}>
-                <input
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input
                     type='text' 
+                    name='title'
                     placeholder='Digite uma nota'
-                    onChange={handleTitle} 
-                    value={title}
-                    autoFocus 
+                    ref={register({ required: true })}
+                    error={errors.title} 
+                    autoFocus
                 />
             </form>
         </Container>
--- src/components/NoteForm/styles.js
+++ src/components/NoteForm/styles.js
@@ -4,19 +4,20 @@ export const Container = styled.div`
     margin: 20px;
     margin-top: 80px;
     padding-bottom: 20px;
-    border-bottom: .5px solid #ced4da;
+    border-bottom: .5px solid #ced4da;    
+`
 
-    input {
-        display: block;
-        width: 100%;
-        height: 50px;
-        padding: 10px 25px;
-        font-size: 1.15rem;
-        color: #495057;
-        background-color: #fff;
-        border: 1px solid #ced4da;
-        border-radius: 50px;
-        transition: border-color .15s ease-in-out,
-        box-shadow .15s ease-in-out;
-    }
+export const Input = styled.input`
+    display: block;
+    width: 100%;
+    height: 50px;
+    padding: 10px 25px;
+    font-size: 1.15rem;
+    color: #495057;
+    background-color: #fff;
+    border: 1px solid #ced4da;
+    border-radius: 50px;
+    transition: border-color .15s ease-in-out,
+    box-shadow .15s ease-in-out;
+    border-color: ${props => props.error && '#e74c3c'}
 `
